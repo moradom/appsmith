43ae1a91f8b59cbd1d0a83474e3a97f6bea3532b
David Duarte <david.m.duarte@trustsystems.pt>
2024-06-07 10:53:03 +0100

    fix version request update

--- app/client/src/sagas/WebsocketSagas/versionUpdatePrompt.ts
+++ app/client/src/sagas/WebsocketSagas/versionUpdatePrompt.ts
@@ -59,46 +59,7 @@ export async function handleVersionUpdate(
   currentVersionData: AppVersionData,
   serverVersion: string,
 ) {
-  const { edition, id: currentVersion } = currentVersionData;
   // If no version is set, ignore
-  if (!currentVersion) return;
-  const versionState: VersionUpdateState | null = await getVersionUpdateState();
-  if (currentVersion === serverVersion) {
-    if (versionState) {
-      AnalyticsUtil.logEvent("VERSION_UPDATE_SUCCESS", {
-        fromVersion: versionState.currentVersion,
-        toVersion: versionState.upgradeVersion,
-        edition,
-      });
-      await removeVersionUpdateState();
-    }
-  }
-  if (currentVersion !== serverVersion) {
-    if (versionState) {
-      timesShown = versionState.timesShown;
-      if (
-        currentVersion === versionState.currentVersion &&
-        versionState.event === UpdateStateEvent.UPDATE_REQUESTED
-      ) {
-        AnalyticsUtil.logEvent("VERSION_UPDATED_FAILED", {
-          fromVersion: versionState.currentVersion,
-          toVersion: versionState.upgradeVersion,
-          edition,
-        });
-      }
-    }
-    const newUpdateState: VersionUpdateState = {
-      currentVersion,
-      upgradeVersion: serverVersion,
-      // Increment the timesShown counter
-      timesShown: timesShown + 1,
-      event: UpdateStateEvent.PROMPT_SHOWN,
-    };
-    AnalyticsUtil.logEvent("VERSION_UPDATE_SHOWN", {
-      fromVersion: currentVersion,
-      toVersion: serverVersion,
-      timesShown,
-    });
-    showPrompt(newUpdateState);
-  }
+    return
 }
+
